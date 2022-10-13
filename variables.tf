a09c5365a16c55efa69d57e72cba83db5d1d9d8e
Joachim Rorbol <jr@efio.dk>
2022-10-13 21:39:04 +0200

    Added default values to config

--- variables.tf
+++ variables.tf
@@ -2,10 +2,10 @@ variable "config" {
   description = ""
   type = object({
     domain_name         = string
-    domain_alias        = optional(set(string))
+    domain_alias        = optional(set(string), [])
     index_document      = optional(string, "index.html")
     error_document      = optional(string, "index.html")
-    allowed_ip_cidrs    = optional(set(string))
+    allowed_ip_cidrs    = optional(set(string), [])
     cache_policy        = optional(string, "CachingOptimized")
     acm_certificate_arn = optional(string)
     disable_cloudfront  = optional(bool, false)
