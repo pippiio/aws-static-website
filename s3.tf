5839cedfdd12fbfc1f6f4a17c46b260fd1ba67a7
Joachim Rorbol <jr@efio.dk>
2023-10-15 21:17:26 +0200

    Removed bucket acl

--- s3.tf
+++ s3.tf
@@ -30,11 +30,6 @@ resource "aws_s3_bucket_public_access_block" "this" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
